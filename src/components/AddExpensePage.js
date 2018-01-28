d9625277e49df145007b00dd1a916f7a0c774880
eprince1 <eitanaprince@gmail.com>
2018-01-28 14:17:40 -0500

    first commit

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
+
+const AddExpensePage = (props) => (
+    <div>
+        <h1>Expense Form</h1>
+        <ExpenseForm 
+            text={'Add Expense'}
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense));
+                props.history.push('/');
+            }}
+        />
+    </div>
+);
+
+export default connect()(AddExpensePage);
---
+++ src/playground/destruct.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'Eitan',
+    age: 21,
+    location: {
+        city: 'UMD',
+        temp: 55
+    }
+};
+
+const {name, age} = person;
